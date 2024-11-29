119f4aa87830312554e00b671cb8f2609f38cb45
Padhi98 <padhiankita1@gmail.com>
2024-11-29 20:46:15 +0530

    First commit

---
+++ hello-world/first.tf
@@ -0,0 +1,10 @@
+// block "label1" label2 {
+    // identifier = expression
+//}
+
+// this is the first comment
+# this is the second comment
+
+output hello1 {
+    value = "Hello World! This is my first file"
+}
